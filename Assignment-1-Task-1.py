4aed78809c980c56a88695466dcae10e86fe1676
myAIML418 <myAIML418@gmail.com>
2026-02-12 15:52:55 +0530

    Commit message

---
+++ Assignment-1-Task-1.py
@@ -0,0 +1,17 @@
+"""Takes two numbers as input from the user"""
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+"""Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+"""
+numAdd = int(num1) + int(num2)
+print ("Addition: ", numAdd)
+numSub = int(num1) - int(num2)
+print ("Subtraction: ", numSub)
+numMult = int(num1) * int(num2)
+print ("Multiplication: ", numMult)
+numDiv = int(num1) / int(num2)
+print ("Division: ", numDiv)
